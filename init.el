71d6148dc1ba9d99782fc4820e7c4f903ed7a4e6
Kyle Sexton <ks@mocker.org>
2016-06-29 09:46:07 -0500

    Initial commit

---
+++ init.el
@@ -0,0 +1,14 @@
+(package-initialize)
+
+(add-to-list 'load-path "/usr/local/share/emacs/site-lisp/")
+
+(let ((default-directory "/usr/local/share/emacs/site-lisp/"))
+  (normal-top-level-add-subdirs-to-load-path))
+
+(when (equal system-type 'darwin)
+  (setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
+  (push "/usr/local/bin" exec-path)
+  (push "/usr/local/texlive/2015/bin/x86_64-darwin/" exec-path)
+  )
+
+(org-babel-load-file "~/.emacs.d/kes.org")
